10dee15bd64c3ce312666b72449466aa78bd3fd8
anbuter <tuberin5@gmail.com>
2019-09-09 20:02:39 +0200

    Added tp, fn, fp, tn metrics to all models

--- complex_models.py
+++ complex_models.py
@@ -5,6 +5,7 @@ import scipy.ndimage.interpolation as ip
 from matplotlib import cm
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix
 
 def augment_image (binim):
@@ -45,15 +46,25 @@ def create_dataset (augment = False):
     return X, Y
 
 def train_model (X, Y, state, alpha):
-    model = MLPClassifier(hidden_layer_sizes = (100,))
+    model = MLPClassifier()
     x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = state)
     model.fit(x_train, y_train)
     score = model.score(x_test, y_test)
-    print(score)
-    return model, score
+    y_test = np.array(np.reshape(y_test, len(y_test))).flatten()
+    tn, fp, fn, tp = confusion_matrix(y_test, model.predict(x_test), labels = [0, 1]).ravel() / len(y_test)
+    print(str(score) + " " + str(tn) + " " + str(fp) + " " + str(fn) + " " + str(tp))
+    return model, score, tn, fp, fn, tp
     
 X, Y = create_dataset(augment = True)
 total_score = 0
+total_tn = 0
+total_fp = 0
+total_fn = 0
+total_tp = 0
 for i in range(10):
-    model, score = train_model(X, Y, i, 0.5)
+    model, score, tn, fp, fn, tp = train_model(X, Y, i, 0.5)
     total_score += score
+    total_tn += tn
+    total_fp += fp
+    total_fn += fn
+    total_tp += tp
--- simple_models.py
+++ simple_models.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
+from sklearn.metrics import confusion_matrix
 
 def fetch_data (path):
     n = 0
@@ -47,18 +48,37 @@ def train_models (X, Y, classifier, state):
     n = len(X)
     models = []
     scores = np.zeros(n)
+    tns = np.zeros(n)
+    fps = np.zeros(n)
+    fns = np.zeros(n)
+    tps = np.zeros(n)
     for i in range(n):
         x_train, x_test, y_train, y_test = train_test_split(X[i], Y[i], test_size = 0.2, random_state = state)
         model = classifier()
         model.fit(x_train, y_train)
         score = model.score(x_test, y_test)
-        print(score)
+        y_test = np.array(np.reshape(y_test, len(y_test))).flatten()
+        tn, fp, fn, tp = confusion_matrix(y_test, model.predict(x_test), labels = [0, 1]).ravel() / len(y_test)
+        print(str(score) + " " + str(tn) + " " + str(fp) + " " + str(fn) + " " + str(tp))
         models.append(model)
         scores[i] = score
-    return models, scores
+        tns[i] = tn
+        fps[i] = fp
+        fns[i] = fn
+        tps[i] = tp
+        
+    return models, scores, tns, fps, fns, tps
 
 X, Y = create_dataset()
 total_scores = np.zeros(len(X))
+total_tns = np.zeros(len(X))
+total_fps = np.zeros(len(X))
+total_fns = np.zeros(len(X))
+total_tps = np.zeros(len(X))
 for i in range(10):
-    models, scores = train_models(X, Y, DecisionTreeClassifier, i)
+    models, scores, tns, fps, fns, tps = train_models(X, Y, LogisticRegression, i)
     total_scores += scores
+    total_tns += tns
+    total_fps += fps
+    total_fns += fns
+    total_tps += tps
